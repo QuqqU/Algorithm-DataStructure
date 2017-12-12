1eab37ef6bec2039beccdac21780f48145dc638d
QuqqU <rldnd913@naver.com>
2017-12-12 18:18:29 +0900

    Create UnionFind.swift

---
+++ UnionFind.swift
@@ -0,0 +1,72 @@
+//
+//  main.swift
+//  unionFind
+//
+//  Created by KiUng Jung on 2017. 12. 12..
+//  Copyright © 2017 KiUng Jung. All rights reserved.
+//
+
+import UIKit
+
+
+class UnionFind {
+    var setCount:Int
+    var largestSize:Int
+    var count:Int
+    var parent = [Int]()
+    var rank = [Int]()
+    var size = [Int]()
+    
+    
+    init?(size:Int) {
+        if(size < 0) {
+            return nil
+        }
+        self.setCount = size
+        self.largestSize = 1
+        self.count = size
+        self.parent = Array(0..<size)
+        self.rank = Array(repeatElement(1, count: size))
+        self.size = Array(repeatElement(1, count: size))
+    }
+
+    @discardableResult func find(parentOf me:Int) -> Int {
+        if me < 0 || count <= me {
+            return -1
+        }
+        if me == self.parent[me] {
+            return me
+        }
+        self.parent[me] = find(parentOf: parent[me])
+        return self.parent[me]
+    }
+    
+    @discardableResult func merge(setOf a:Int, setOf b:Int) -> Bool {
+        if a < 0 || count <= a || b < 0 || count <= b {
+            return false
+        }
+        
+        var a = find(parentOf: a)
+        var b = find(parentOf: b)
+        
+        if a == b {
+            return false
+        }
+        
+        if self.size[a] < self.size[b] {
+            swap(&a, &b)
+        }
+        self.parent[b] = a
+        if self.rank[a] == self.rank[b] {
+            self.rank[a] += 1
+        }
+        
+        self.setCount -= 1
+        self.size[a] += self.size[b]
+        self.largestSize = max(self.largestSize, self.size[a])
+        
+        return true
+    }
+}
+
+
